d3239ccd51715549d749437c4ca2a08d1c5bf2ff
Jesse <99794044+ColonJesseJ@users.noreply.github.com>
2025-08-29 18:38:09 -0700

    fixed tab

--- init.lua
+++ init.lua
@@ -121,6 +121,12 @@ end)
 -- Enable break indent
 vim.o.breakindent = true
 
+-- fixes tabs
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+
 -- Save undo history
 vim.o.undofile = true
 
